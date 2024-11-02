0dd96fd2e3d19d049bf5bfbfafbedf9d71f67374
Hunter Williams <hunter@reusserdesign.com>
2024-11-01 20:55:44 -0500

    fetch teams

--- src/components/Wrapper.jsx
+++ src/components/Wrapper.jsx
@@ -1,93 +1,37 @@
 import React from 'react';
-import {
-  Disclosure,
-  DisclosureButton,
-  DisclosurePanel,
-} from '@headlessui/react';
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
-
-const navigation = [
-  { name: 'Dashboard', href: '/', current: true },
-  { name: 'Teams', href: '/teams', current: false },
-];
-
-function classNames(...classes) {
-  return classes.filter(Boolean).join(' ');
-}
+import Navigation from './Navigation';
+import { useQuery } from 'react-query';
 
 function Wrapper() {
+  const {
+    isLoading,
+    isSuccess,
+    isError,
+    data: teams,
+  } = useQuery('teams', fetchTeams);
+
+  function fetchTeams() {
+    return fetch(
+      'https://nfl-api-data.p.rapidapi.com/nfl-team-listing/v1/data',
+      {
+        method: 'GET',
+        headers: {
+          'x-rapidapi-key':
+            'bb199be5d2mshdea9eec3806d994p1640d1jsn12f149376f53',
+          'x-rapidapi-host': 'nfl-api-data.p.rapidapi.com',
+        },
+      }
+    )
+      .then(response => response.json())
+      .then(data => {
+        // console.log(data);
+        return data.map(item => item.team);
+      });
+  }
+
   return (
     <div className="min-h-full">
-      <Disclosure as="nav" className="bg-gray-800">
-        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-          <div className="flex h-16 items-center justify-between">
-            <div className="flex items-center">
-              <div className="flex-shrink-0">
-                <img
-                  alt="Your Company"
-                  src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=500"
-                  className="h-8 w-8"
-                />
-              </div>
-              <div className="hidden md:block">
-                <div className="ml-10 flex items-baseline space-x-4">
-                  {navigation.map(item => (
-                    <a
-                      key={item.name}
-                      href={item.href}
-                      aria-current={item.current ? 'page' : undefined}
-                      className={classNames(
-                        item.current
-                          ? 'bg-gray-900 text-white'
-                          : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                        'rounded-md px-3 py-2 text-sm font-medium'
-                      )}
-                    >
-                      {item.name}
-                    </a>
-                  ))}
-                </div>
-              </div>
-            </div>
-            <div className="-mr-2 flex md:hidden">
-              {/* Mobile menu button */}
-              <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                <span className="absolute -inset-0.5" />
-                <span className="sr-only">Open main menu</span>
-                <Bars3Icon
-                  aria-hidden="true"
-                  className="block h-6 w-6 group-data-[open]:hidden"
-                />
-                <XMarkIcon
-                  aria-hidden="true"
-                  className="hidden h-6 w-6 group-data-[open]:block"
-                />
-              </DisclosureButton>
-            </div>
-          </div>
-        </div>
-
-        <DisclosurePanel className="md:hidden">
-          <div className="space-y-1 px-2 pb-3 pt-2 sm:px-3">
-            {navigation.map(item => (
-              <DisclosureButton
-                key={item.name}
-                as="a"
-                href={item.href}
-                aria-current={item.current ? 'page' : undefined}
-                className={classNames(
-                  item.current
-                    ? 'bg-gray-900 text-white'
-                    : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                  'block rounded-md px-3 py-2 text-base font-medium'
-                )}
-              >
-                {item.name}
-              </DisclosureButton>
-            ))}
-          </div>
-        </DisclosurePanel>
-      </Disclosure>
+      <Navigation />
 
       <header className="bg-white shadow">
         <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
@@ -104,6 +48,19 @@ function Wrapper() {
             <li>Teams API integration</li>
             <li>Players API integration</li>
           </ul>
+          <br />
+
+          <div>
+            {isLoading && <p>Loading...</p>}
+            {isError && <p>Error fetching teams</p>}
+            {isSuccess && (
+              <ul>
+                {teams.map(team => (
+                  <li key={team.id}>{team.displayName}</li>
+                ))}
+              </ul>
+            )}
+          </div>
         </div>
       </main>
     </div>
