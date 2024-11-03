13a9c223d9ed826b0cbf4ac2e7cff510eb0c3a27
Hunter Williams <hunter@reusserdesign.com>
2024-11-02 19:05:54 -0500

    set stale time and cache time

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,7 +5,17 @@ import { ReactQueryDevtools } from 'react-query/devtools';
 // const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }) {
-  const [queryClient] = useState(() => new QueryClient());
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 1000 * 60 * 60 * 48, // 48 hours (1 day) - Adjust as needed
+            cacheTime: 1000 * 60 * 60 * 24 * 7, // 7 days (1 week)
+          },
+        },
+      })
+  );
 
   return (
     <QueryClientProvider client={queryClient}>
