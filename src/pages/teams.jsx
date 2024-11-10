b970144f7d0dcc6248cc743d7137d748792e8e17
Hunter Williams <hunter@reusserdesign.com>
2024-11-09 21:28:36 -0600

    comment out api fetch for now

--- src/pages/teams.jsx
+++ src/pages/teams.jsx
@@ -11,43 +11,44 @@ function Teams() {
   } = useQuery('teams', fetchTeams);
 
   function fetchTeams() {
-    return fetch(
-      'https://nfl-api-data.p.rapidapi.com/nfl-team-listing/v1/data',
-      {
-        method: 'GET',
-        headers: {
-          'x-rapidapi-key':
-            'bb199be5d2mshdea9eec3806d994p1640d1jsn12f149376f53',
-          'x-rapidapi-host': 'nfl-api-data.p.rapidapi.com',
-        },
-      }
-    )
-      .then(response => response.json())
-      .then(data => {
-        return data.map(item => item.team);
-      });
+    console.log('fetchTeams');
+    return;
+    // return fetch(
+    //   'https://nfl-api-data.p.rapidapi.com/nfl-team-listing/v1/data',
+    //   {
+    //     method: 'GET',
+    //     headers: {
+    //       'x-rapidapi-key':
+    //         'bb199be5d2mshdea9eec3806d994p1640d1jsn12f149376f53',
+    //       'x-rapidapi-host': 'nfl-api-data.p.rapidapi.com',
+    //     },
+    //   }
+    // )
+    //   .then(response => response.json())
+    //   .then(data => {
+    //     return data.map(item => item.team);
+    //   });
   }
   return (
     <Layout>
+      <header className="bg-white shadow">
+        <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+          <h1 className="text-3xl font-bold tracking-tight text-gray-900">
+            Teams
+          </h1>
+        </div>
+      </header>
       <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        <h3>Todos</h3>
-        <ul>
-          <li>Routing with Next.js</li>
-          <li>Teams API integration</li>
-          <li>Players API integration</li>
-        </ul>
-        <br />
-
         <div>
           {isLoading && <p>Loading...</p>}
           {isError && <p>Error fetching teams</p>}
-          {isSuccess && (
+          {/* {isSuccess && (
             <ul>
               {teams.map(team => (
                 <li key={team.id}>{team.displayName}</li>
               ))}
             </ul>
-          )}
+          )} */}
         </div>
       </div>
     </Layout>
