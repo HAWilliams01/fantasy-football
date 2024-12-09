4ea08858ac9756fcdb83a96b4b053d56842c584b
Hunter Williams <hunter@reusserdesign.com>
2024-12-08 21:48:15 -0600

    get players page api working

--- src/pages/players.jsx
+++ src/pages/players.jsx
@@ -4,44 +4,40 @@ import Layout from '../components/Layout';
 
 function Players() {
   const [searchQuery, setSearchQuery] = useState('');
-  const [isSuccess, setIsSuccess] = useState(true);
-  //   const {
-  //     isLoading,
-  //     isSuccess,
-  //     isError,
-  //     data: players,
-  //   } = useQuery('players', fetchPlayers);
-
-  const players = [
-    { id: 1, name: 'Tom Brady' },
-    { id: 2, name: 'Aaron Rodgers' },
-    { id: 3, name: 'Patrick Mahomes' },
-    { id: 4, name: 'Josh Allen' },
-    { id: 5, name: 'Lamar Jackson' },
-  ];
+  const {
+    isLoading,
+    isSuccess,
+    isError,
+    data: players,
+  } = useQuery('players', fetchPlayers);
 
   function fetchPlayers() {
     console.log('fetchPlayers');
-    return;
-    // return fetch(
-    //   'https://nfl-api-data.p.rapidapi.com/nfl-team-listing/v1/data',
-    //   {
-    //     headers: {
-    //       'x-rapidapi-key':
-    //         'bb199be5d2mshdea9eec3806d994p1640d1jsn12f149376f53',
-    //       'x-rapidapi-host': 'nfl-api-data.p.rapidapi.com',
-    //     },
-    //   }
-    // )
-    //   .then(response => response.json())
-    //   .then(data => {
-    //     return data;
-    //   });
+
+    return fetch(
+      'https://tank01-nfl-live-in-game-real-time-statistics-nfl.p.rapidapi.com/getNFLProjections?week=season&archiveSeason=2024&twoPointConversions=2&passYards=.04&passAttempts=-.5&passTD=4&passCompletions=1&passInterceptions=-2&pointsPerReception=1&carries=.2&rushYards=.1&rushTD=6&fumbles=-2&receivingYards=.1&receivingTD=6&targets=.1&fgMade=3&fgMissed=-1&xpMade=1&xpMissed=-1',
+      {
+        headers: {
+          'x-rapidapi-key':
+            'bb199be5d2mshdea9eec3806d994p1640d1jsn12f149376f53',
+          'x-rapidapi-host':
+            'tank01-nfl-live-in-game-real-time-statistics-nfl.p.rapidapi.com',
+        },
+      }
+    )
+      .then(response => response.json())
+      .then(data => {
+        // console.log('API Response:', data);
+        return Object.values(data?.body?.playerProjections || {});
+      });
   }
 
-  const filteredPlayers = players?.filter(player =>
-    player.name.toLowerCase().includes(searchQuery.toLowerCase())
-  );
+  const filteredPlayers = players?.filter(player => {
+    // console.log('Player:', player);
+    return player.longName?.toLowerCase().includes(searchQuery.toLowerCase());
+  });
+
+  console.log('Filtered Players:', filteredPlayers);
 
   return (
     <Layout>
@@ -54,6 +50,12 @@ function Players() {
       </header>
       <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
         <div>
+          <div className="text-white">
+            {/* Ideal buttons:
+              Sort A - Z by default
+              Give option for Z - A */}
+            Filter by team dropdown
+          </div>
           <input
             type="text"
             placeholder="Search players"
@@ -61,15 +63,20 @@ function Players() {
             onChange={e => setSearchQuery(e.target.value)}
             className="mb-4 p-2 border border-gray-300 rounded"
           />
-          {/* {isLoading && <p>Loading...</p>} */}
-          {/* {isError && <p>Error fetching players</p>} */}
-          {isSuccess && filteredPlayers.length > 0 && (
-            <ul>
-              {filteredPlayers.map(player => (
-                <li key={player.id}>{player.name}</li>
-              ))}
-            </ul>
-          )}
+          <div className="text-white">
+            {!isLoading && <p>NOT Loading...</p>}
+            {isLoading && <p>Loading...</p>}
+            {isError && <p>Error fetching players</p>}
+            {isSuccess && filteredPlayers?.length > 0 && (
+              <ul>
+                {filteredPlayers.map(player => (
+                  <li key={player.id} className="text-white text-lg">
+                    {player.longName} - {player.fantasyPointsDefault.PPR}
+                  </li>
+                ))}
+              </ul>
+            )}
+          </div>
         </div>
       </div>
     </Layout>
